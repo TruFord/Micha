96104f5b294e0be993f24a154bbc003b805e9e13
Joshua Konstantinos <Josh@dragon3dsolutions.com>
2017-12-07 22:02:07 -0500

    working Eyes
    Eyes working, Motors working and driving straight.

--- MichaDemo.ino
+++ MichaDemo.ino
@@ -9,6 +9,14 @@
   http://www.arduino.cc/en/Tutorial/Sweep
 */
 
+//define pin name
+#define dir_1 11
+#define pwm_1 10
+#define dir_2 8
+#define pwm_2 9
+
+
+
 #include "LedControl.h"
 
 /*
@@ -19,7 +27,8 @@
  pin 10 is connected to LOAD 
  We have only a single MAX72XX.
  */
-LedControl lc=LedControl(32,36,34,1);
+LedControl lc2=LedControl(32,36,34,2);
+LedControl lc1=LedControl(52,50,48,2);
 
 /* we always wait a bit between updates of the display */
 unsigned long delaytime=100;
@@ -38,6 +47,15 @@ int nintydeg = 90;
 int opengrip = 0; // variable to store the servo position
 
 void setup() {
+
+
+   //declare pins as INPUT/OUTPUT 
+  pinMode(pwm_1,OUTPUT);
+  pinMode(dir_1,OUTPUT);
+  pinMode(pwm_2,OUTPUT);
+  pinMode(dir_2,OUTPUT);
+
+  
   neckservo.attach(33);  // attaches the servo on pin 9 to the servo object
   chinservo.attach(35);
   gripservo.attach(37); // 0 deg is open 90+ is closed
@@ -47,11 +65,17 @@ void setup() {
    The MAX72XX is in power-saving mode on startup,
    we have to do a wakeup call
    */
-  lc.shutdown(0,false);
+  lc2.shutdown(0,false);
   /* Set the brightness to a medium values */
-  lc.setIntensity(0,8);
+  lc2.setIntensity(0,8);
   /* and clear the display */
-  lc.clearDisplay(0);
+  lc2.clearDisplay(0);
+
+   lc1.shutdown(0,false);
+  /* Set the brightness to a medium values */
+  lc1.setIntensity(0,8);
+  /* and clear the display */
+  lc1.clearDisplay(0);
   
 
 }
@@ -61,38 +85,50 @@ void loop() {
  {
 neutral();
 }
-chinservo.write(80);
-rotategripservo.write(90);
-delay(1000);
-rotategripservo.write(34);
-delay(1000);
-rotategripservo.write(90);
-gripservo.write(0);
-delay(4500);
-gripservo.write(90);
-neckservo.write(30);              // tell servo to go to position in variable 'pos'
-delay(6000);                       // waits 15ms for the servo to reach the position
-neckservo.write(90);              // tell servo to go to position in variable 'pos'
+//chinservo.write(80);
+//rotategripservo.write(90);
+//delay(1000);
+//rotategripservo.write(34);
+//delay(1000);
+//rotategripservo.write(90);
+//gripservo.write(0);
+
+ // digitalWrite(dir_1,HIGH);    //controls the direction the motor
+//digitalWrite(dir_2,HIGH);
+//analogWrite(pwm_2,200);         //increase the speed of the motor from 0 to 255
+ // analogWrite(pwm_1, 150);  //decrease the speed of the motor from 255 to 0
+
+
+
+//delay(4500);
+//gripservo.write(90);
+//neckservo.write(30);              // tell servo to go to position in variable 'pos'
+//delay(6000);                       // waits 15ms for the servo to reach the position
+//neckservo.write(90);              // tell servo to go to position in variable 'pos'
 delay(4000);
-chinservo.write(120);
+//chinservo.write(120);
 delay(4000);
 }
 
 void neutral()
 {
-lc.setRow(0,0,60);
-lc.setRow(0,1,126);
-lc.setRow(0,2,102);
-lc.setRow(0,3,102);
-lc.setRow(0,4,102);
-lc.setRow(0,5,126);
-lc.setRow(0,6,60);
-lc.setRow(1,1,60);
-lc.setRow(1,2,126);
-lc.setRow(1,3,102);
-lc.setRow(1,4,102);
-lc.setRow(1,5,102);
-lc.setRow(1,6,126);
-lc.setRow(1,7,60);
+lc2.setRow(0,0,60);
+lc2.setRow(0,1,126);
+lc2.setRow(0,2,102);
+lc2.setRow(0,3,102);
+lc2.setRow(0,4,102);
+lc2.setRow(0,5,126);
+lc2.setRow(0,6,60);
+
+lc1.setRow(0,0,60);
+lc1.setRow(0,1,126);
+lc1.setRow(0,2,102);
+lc1.setRow(0,3,102);
+lc1.setRow(0,4,102);
+lc1.setRow(0,5,126);
+lc1.setRow(0,6,60);
+
+
+
 delay(50);
 }
