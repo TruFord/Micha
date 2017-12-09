d00e0b86e4727f566147664e2193ffc945a6600c
Joshua Konstantinos <Josh@dragon3dsolutions.com>
2017-12-09 16:10:54 -0500

    current version line follower

--- MostBasicFollowerMICHA.ino
+++ MostBasicFollowerMICHA.ino
@@ -57,6 +57,35 @@ SensorBar mySensorBar(SX1509_ADDRESS);
 
 uint8_t state;
 
+#include "LedControl.h"
+
+/*
+ Now we need a LedControl to work with.
+ ***** These pin numbers will probably not work with your hardware *****
+ pin 12 is connected to the DataIn 
+ pin 11 is connected to the CLK 
+ pin 10 is connected to LOAD 
+ We have only a single MAX72XX.
+ */
+LedControl lc2=LedControl(32,36,34,2);
+LedControl lc1=LedControl(52,50,48,2);
+
+/* we always wait a bit between updates of the display */
+unsigned long delaytime=100;
+
+
+#include <Servo.h>
+
+Servo neckservo;  // create servo object to control a servo
+Servo chinservo; // create servo to control wrist on gripper
+Servo gripservo; // create servo to control wrist on gripper
+Servo rotategripservo;
+
+int closedgrip = 90;    // variable to store the servo position
+
+int nintydeg = 90;
+int opengrip = 0; // variable to store the servo position
+
 
 void setup()
 {
@@ -92,23 +121,65 @@ void setup()
   pinMode(pwm_2,OUTPUT);
   pinMode(dir_2,OUTPUT);
 
+ neckservo.attach(33);  // attaches the servo on pin 9 to the servo object
+  chinservo.attach(35);
+  gripservo.attach(37); // 0 deg is open 90+ is closed
+  rotategripservo.attach(39); //34 is to the right
+
+   /*
+   The MAX72XX is in power-saving mode on startup,
+   we have to do a wakeup call
+   */
+  lc2.shutdown(0,false);
+  /* Set the brightness to a medium values */
+  lc2.setIntensity(0,8);
+  /* and clear the display */
+  lc2.clearDisplay(0);
+
+   lc1.shutdown(0,false);
+  /* Set the brightness to a medium values */
+  lc1.setIntensity(0,8);
+  /* and clear the display */
+  lc1.clearDisplay(0);
+  
+
+
+
 
 }
 
 void loop()
 {
+lc2.setRow(0,0,60);
+lc2.setRow(0,1,126);
+lc2.setRow(0,2,102);
+lc2.setRow(0,3,102);
+lc2.setRow(0,4,102);
+lc2.setRow(0,5,126);
+lc2.setRow(0,6,60);
 
+lc1.setRow(0,0,60);
+lc1.setRow(0,1,126);
+lc1.setRow(0,2,102);
+lc1.setRow(0,3,102);
+lc1.setRow(0,4,102);
+lc1.setRow(0,5,126);
+lc1.setRow(0,6,60);
 
   uint8_t nextState = state;
   switch (state) {
-    case IDLE_STATE:                  //stops both motors
+    case IDLE_STATE:   //stops both motors
+
+     Serial.println("STATE: IDLE");
+    
       digitalWrite(dir_1, HIGH);    //controls the direction the motor HIGH = Forward
       digitalWrite(dir_2, HIGH);    //controls the direction the motor HIGH = Forward
-      analogWrite(pwm_2, 127);        //increase the speed of the motor from 0 to 255
-      analogWrite(pwm_1, 127);        //decrease the speed of the motor from 255 to 0;
+      analogWrite(pwm_2, 0);        //increase the speed of the motor from 0 to 255
+      analogWrite(pwm_1, 0);        //decrease the speed of the motor from 255 to 0;
       nextState = READ_LINE;
       break;
     case READ_LINE:
+        Serial.println("STATE: read line");
       if ( mySensorBar.getDensity() < 7 )
       {
         nextState = GO_FORWARD;
@@ -127,24 +198,40 @@ void loop()
       }
       break;
     case GO_FORWARD:
+
+
+
+ 
+      neckservo.write(90);              // tell servo to go to position in variable 'pos'
+
+      Serial.println("case: go forward");
       digitalWrite(dir_1, HIGH);    //controls the direction the motor HIGH = Forward
       digitalWrite(dir_2, HIGH);    //controls the direction the motor HIGH = Forward
-      analogWrite(pwm_2, 200);        //increase the speed of the motor from 0 to 255
-      analogWrite(pwm_1, 150);        //decrease the speed of the motor from 255 to 0;    POWER SENT TO RIGHT TRACK MUST BE ~75% POWER BECASUE OF HIGH FRICTION ON LEFT SIDE.
+      analogWrite(pwm_2, 100);        //increase the speed of the motor from 0 to 255
+      analogWrite(pwm_1, 100);        //decrease the speed of the motor from 255 to 0;    POWER SENT TO RIGHT TRACK MUST BE ~75% POWER BECASUE OF HIGH FRICTION ON LEFT SIDE.
       nextState = READ_LINE;
       break;
     case GO_LEFT:
+
+  
+    neckservo.write(150);              // tell servo to go to position in variable 'pos'
+
+     Serial.println("case: go left");
       digitalWrite(dir_1, HIGH);    //controls the direction the motor HIGH = Forward
       digitalWrite(dir_2, HIGH);    //controls the direction the motor HIGH = Forward
-      analogWrite(pwm_2, 200);        //increase the speed of the motor from 0 to 255
-      analogWrite(pwm_1, 200);        //decrease the speed of the motor from 255 to 0;    POWER SENT TO RIGHT TRACK MUST BE ~75% POWER BECASUE OF HIGH FRICTION ON LEFT SIDE.
+      analogWrite(pwm_2, 100);        //increase the speed of the motor from 0 to 255
+      analogWrite(pwm_1, 0);        //decrease the speed of the motor from 255 to 0;    POWER SENT TO RIGHT TRACK MUST BE ~75% POWER BECASUE OF HIGH FRICTION ON LEFT SIDE.
       nextState = READ_LINE;
       break;
     case GO_RIGHT:
+
+    
+    neckservo.write(30);              // tell servo to go to position in variable 'pos'
+    Serial.println("cdase: go right");
       digitalWrite(dir_1, HIGH);    //controls the direction the motor HIGH = Forward
       digitalWrite(dir_2, HIGH);    //controls the direction the motor HIGH = Forward
-      analogWrite(pwm_2, 200);        //increase the speed of the motor from 0 to 255
-      analogWrite(pwm_1, 112);        //decrease the speed of the motor from 255 to 0;    POWER SENT TO RIGHT TRACK MUST BE ~75% POWER BECASUE OF HIGH FRICTION ON LEFT SIDE.
+      analogWrite(pwm_2, 0);        //increase the speed of the motor from 0 to 255
+      analogWrite(pwm_1, 100);        //decrease the speed of the motor from 255 to 0;    POWER SENT TO RIGHT TRACK MUST BE ~75% POWER BECASUE OF HIGH FRICTION ON LEFT SIDE.
 
       nextState = READ_LINE;
       break;
@@ -153,11 +240,19 @@ void loop()
       digitalWrite(dir_2, HIGH);    //controls the direction the motor HIGH = Forward
       analogWrite(pwm_2, 0);        //increase the speed of the motor from 0 to 255
       analogWrite(pwm_1, 0);        //decrease the speed of the motor from 255 to 0;
+      
       break;
   }
   state = nextState;
-  //delay(100);
+  delay(100);
+
+
+
+
 }
+
+
+
 
 
 
